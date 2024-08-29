663644c1f55bc07a65869910970b9666a6aa592a
rkozhan~ <yalta.vrk@gmail.com>
2024-08-29 17:08:03 +0200

    testing index ActionResult

--- LibraryApp.Test/BooksControllerTest.cs
+++ LibraryApp.Test/BooksControllerTest.cs
@@ -1,14 +1,34 @@
+using Library.API.Data.Models;
+using Library.API.Data.Services;
+using LibraryApp.Controllers;
+using LibraryApp.Data.MockData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LibraryApp.Test
 {
-    public class UnitTest1
+    public class BooksControllerTest
     {
         [Fact]
-        public void Test1()
+        public void IndexUnitTest()
         {
+            //arrange
+            var mockRepo = new Mock<IBookService>();
+            mockRepo.Setup(n => n.GetAll()).Returns(MockData.GetTestBookItems());
+            var controller = new BooksController(mockRepo.Object);
 
+            //act
+            var result = controller.Index();
+
+            //assert
+            Assert.IsType<ViewResult>(result);
+            var viewResult = result as ViewResult;
+            Assert.IsAssignableFrom<List<Book>>(viewResult.ViewData.Model);
+            var viewResultBooks = viewResult.ViewData.Model as List<Book>;
+            Assert.Equal(5, viewResultBooks.Count);
         }
     }
 }
